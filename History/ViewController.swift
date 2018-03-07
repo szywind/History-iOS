04516a978ebbd79bf9beff7814c516dd9ac1d99d
Zhenyuan Shen <szywind@163.com>
2018-03-07 17:41:21 +0800

    Initial Commit

---
+++ History/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  History
+//
+//  Created by Cloudream on 07/03/2018.
+//  Copyright © 2018 GSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
