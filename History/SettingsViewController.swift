8fb020bed62c07d50610aba9c904d00f183e3188
Zhenyuan Shen <szywind@163.com>
2018-03-08 18:24:51 +0800

    add some vc

---
+++ History/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  History
+//
+//  Created by Cloudream on 08/03/2018.
+//  Copyright © 2018 GSS. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func OnClickRegisterOrLogin(_ sender: UIButton) {
+        // TODO
+        // https://github.com/leancloud/leancloud-social-ios
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
