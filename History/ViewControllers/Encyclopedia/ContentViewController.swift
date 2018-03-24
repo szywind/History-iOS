8dc5d1d28a0c2340dc77a8122efa6d96e626d785
szywind@163.com <8891@Szy>
2018-03-24 16:18:09 +0800

    add sorting by Pinyin

--- History/ViewControllers/Encyclopedia/ContentViewController.swift
+++ History/ViewControllers/Encyclopedia/ContentViewController.swift
@@ -11,8 +11,25 @@ import UIKit
 class ContentViewController: UIViewController {
     
     var records = [Record] ()
+    var recordDictionary = [String: [Record]]()
+    var recordSectionTitles = [String]()
     
     @IBOutlet weak var recordTableView: UITableView!
+
+    func processData() {
+        
+        for record in records {
+            let key = record.pinyin?.prefix(1).uppercased()
+            if var value = recordDictionary[key!] {
+                value.append(record)
+                recordDictionary[key!] = value
+            } else {
+                recordDictionary[key!] = [record]
+            }
+        }
+        
+        recordSectionTitles = recordDictionary.keys.sorted(by: { $0 < $1 })
+    }
     
     class func create() -> ContentViewController {
         let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
@@ -23,6 +40,7 @@ class ContentViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        refresh()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +50,7 @@ class ContentViewController: UIViewController {
 
     
     func refresh() {
+        processData()
         recordTableView.reloadData()
     }
     
@@ -58,15 +77,32 @@ class ContentViewController: UIViewController {
 
 extension ContentViewController: UITableViewDelegate, UITableViewDataSource {
     // MARK: - Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return recordDictionary.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return records.count
+        guard let sectionData = recordDictionary[recordSectionTitles[section]] else {
+            return 0
+        }
+        return sectionData.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return recordSectionTitles[section]
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = recordTableView.dequeueReusableCell(withIdentifier: "recordCell") as! RecordTableViewCell
         
-        cell.recordLbl.text = records[indexPath.row].name
-        cell.recordImage.image = records[indexPath.row].avatar
+        // Configure the cell...
+        guard let sectionData = recordDictionary[recordSectionTitles[indexPath.section]] else {
+            return cell
+        }
+        
+        cell.recordLbl.text = sectionData[indexPath.row].name
+        cell.recordImage.image = sectionData[indexPath.row].avatar
         
         return cell
     }
@@ -78,5 +114,9 @@ extension ContentViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         dismissKeyboard()
         performSegue(withIdentifier: "showRecordDetails", sender: self)
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return recordSectionTitles
     }
 }
