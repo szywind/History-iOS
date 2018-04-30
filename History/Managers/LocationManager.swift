94225bc1a5036e06f286a173cc6d3ab10639d623
szywind@163.com <8891@Szy>
2018-04-30 20:43:02 +0800

    add Location Mgt. module

---
+++ History/Managers/LocationManager.swift
@@ -0,0 +1,67 @@
+//
+//  LocationManager.swift
+//  History
+//
+//  Created by Zhenyuan Shen on 4/30/18.
+//  Copyright © 2018 GSS. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+import UIKit
+
+class LocationManager: UIViewController, CLLocationManagerDelegate {
+
+    // handle location http://kristofk.com/location-swift/
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        requestLocation()
+    }
+    
+    func requestLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        State.currentLocation = locations.last
+        if let lat = locations.last?.coordinate.latitude, let long = locations.last?.coordinate.longitude {
+            print("\(lat),\(long)")
+        } else {
+            print("No coordinates")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?) -> Void ) {
+        // Use the last reported location.
+        if let lastLocation = self.locationManager.location {
+            let geocoder = CLGeocoder()
+            
+            // Look up the location and pass it to the completion handler
+            geocoder.reverseGeocodeLocation(lastLocation, completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    completionHandler(firstLocation)
+                }
+                else {
+                    // An error occurred during geocoding.
+                    completionHandler(nil)
+                }
+            })
+        }
+        else {
+            // No location was available.
+            completionHandler(nil)
+        }
+    }
+}
